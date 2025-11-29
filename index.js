8bb4211101e5a6b92a4eeeb0746098024b15f2f4
sushant <sushant@dev.worldlink.com.np>
2025-11-29 15:43:00 +0545

    minor cors bug

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ import UserRouter from "./routes/UserRoute.js";
 import ProductRouter from "./routes/ProductRoute.js";
 import db from "./databaseConnection.js";
 // import "./lowStock.js"
-app.use(cors());
 app.use(
   cors({
     credentials: true,
